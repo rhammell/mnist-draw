cee7b9da39a4eecb4269c1a3dbc12d2bdb64f6aa
Bob Hammell <rhammell@gmail.com>
2017-07-15 17:11:50 -0400

    Status icon and error handling

--- cgi-bin/mnist.py
+++ cgi-bin/mnist.py
@@ -1,20 +1,24 @@
-#!/home/rhammell/Python-3.4.3/python
+"""
+CGI script that accepts image urls and feeds them into a ML classifier. Results
+are returned in JSON format. 
+"""
 
 import io
-import cgi
 import json
 import sys
 import os
-import random
 import re
 import base64
 import numpy as np
 from PIL import Image
 from model import model
 
+# Default output
+res = {"result": 0,
+       "data": [], 
+       "error": ''}
 
 try:
-
     # Get post data
     if os.environ["REQUEST_METHOD"] == "POST":
         data = sys.stdin.read(int(os.environ["CONTENT_LENGTH"]))
@@ -32,17 +36,17 @@ try:
         model.load('cgi-bin/models/model.tfl')
 
         # Predict class
-        prediction = model.predict([arr])[0]
+        predictions = model.predict([arr])[0]
 
-        # Results  
-        res = { "result": 1,
-            "data":  [float(num) for num in prediction],
-            "test": 'test1', 
-            "data2": 'tes',
-            "data3": data } #}
-except:
-     res = { "result": 0 }
+        # Return label data
+        res['result'] = 1
+        res['data'] = [float(num) for num in predictions] 
 
+except Exception as e:
+    # Return error data
+    res['error'] = str(e)
+
+# Print JSON response
 print("Content-type: application/json")
 print("") 
 print(json.dumps(res))
--- js/custom.js
+++ js/custom.js
@@ -4,47 +4,53 @@ canvas.isDrawingMode = true;
 canvas.freeDrawingBrush.width = 20;
 canvas.freeDrawingBrush.color = "#000000";
 canvas.backgroundColor = "#ffffff";
-canvas.renderAll()
+canvas.renderAll();
+
 
 // Clear button callback
 $("#clear-canvas").click(function(){ 
   canvas.clear(); 
   canvas.backgroundColor = "#ffffff";
   canvas.renderAll();
+  updateChart(zeros);
+  $("#status").removeClass();
 });
+
 
 // Predict button callback
 $("#predict").click(function(){  
 
+  // Change status indicator
+  $("#status").removeClass().toggleClass("fa fa-spinner fa-spin");
+
   // Get canvas contents as url
-	var fac = (1.) / 13.; 
-	var url = canvas.toDataURLWithMultiplier('png', fac);
-  console.log(url);
+  var fac = (1.) / 13.; 
+  var url = canvas.toDataURLWithMultiplier('png', fac);
 
   // Post url to python script
-	var jq = $.post('cgi-bin/mnist.py', url)
-		.done(function (json) {
-			if (json.result) {
-			   console.log("OK.")
-         console.log(json)
-         updateChart(json.data)
-			} else {
-         console.log(json)
-			   console.log("API Error: Error occured.");
-			}
-		})
-		.fail(function (jqxhr, textStatus, error) {
-			var err = textStatus + ", " + error;
-			console.log("API Error: " + err);
- 		}
- 	);
+  var jq = $.post('cgi-bin/mnist.py', url)
+    .done(function (json) {
+      if (json.result) {
+        $("#status").removeClass().toggleClass("fa fa-check");
+        $('#svg-chart').show();
+        updateChart(json.data);
+      } else {
+         $("#status").removeClass().toggleClass("fa fa-exclamation-triangle");
+         console.log('Script Error: ' + json.error)
+      }
+    })
+    .fail(function (xhr, textStatus, error) {
+      $("#status").removeClass().toggleClass("fa fa-exclamation-triangle");
+      console.log("POST Error: " + xhr.responseText + ", " + textStatus + ", " + error);
+    }
+  );
 
 });
 
 // Iniitialize d3 bar chart
 $('#svg-chart').hide();
 var labels = ['0','1','2','3','4','5','6','7','8','9'];
-var vals = [0,0,0,0,0,0,0,0,0,0,0];
+var zeros = [0,0,0,0,0,0,0,0,0,0,0];
 
 var margin = {top: 0, right: 0, bottom: 20, left: 0},
     width = 360 - margin.left - margin.right,
@@ -63,7 +69,7 @@ var x = d3.scale.ordinal()
     
 var y = d3.scale.linear()
           .range([height, 0])
-          .domain([0,d3.max(vals)]);  
+          .domain([0,1]);  
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -71,7 +77,7 @@ var xAxis = d3.svg.axis()
     .tickSize(0);
 
 svg.selectAll(".bar")
-    .data(vals)
+    .data(zeros)
   .enter().append("rect")
     .attr("class", "bar")
     .attr("x", function(d, i) { return x(i); })
@@ -86,8 +92,6 @@ svg.append("g")
 
 // Update chart data
 function updateChart(data) {
-  $('#svg-chart').show();
-  y.domain([0,d3.max(data)])
   d3.selectAll("rect")
     .data(data)
     .transition()
